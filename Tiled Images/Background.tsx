6cc2f3aaa46d62b7d299ea23a61fd5e042d1310c
hamtime63 <59403235+hamtime63@users.noreply.github.com>
2021-09-16 19:37:18 +1200

    background tileset

---
+++ Tiled Images/Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Background" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="Dirt2back.png" width="32" height="32"/>
+</tileset>
