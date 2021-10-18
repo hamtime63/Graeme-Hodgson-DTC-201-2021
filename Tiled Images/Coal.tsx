d3cf9a527d418c7e150ff7a8adf19cc4c1005e02
hamtime63 <59403235+hamtime63@users.noreply.github.com>
2021-10-19 09:36:52 +1300

    coal&gold
    #Final commit

--- Tiled Images/Coal.tsx
+++ Tiled Images/Coal.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.0" name="Coal" tilewidth="32" tileheight="32" tilecount="1" columns="1">
  <image source="Coal.png" width="32" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="Points" type="int" value="2"/>
+  </properties>
+ </tile>
 </tileset>
--- Tiled Images/Gold.tsx
+++ Tiled Images/Gold.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.0" name="Gold" tilewidth="32" tileheight="32" tilecount="1" columns="1">
  <image source="Gold.png" width="32" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="Points" type="int" value="4"/>
+  </properties>
+ </tile>
 </tileset>
